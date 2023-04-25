4cc68d08a1170f5f89087cc30f33c04cce6bd7ea
Astel123457 <41224955+Astel123457@users.noreply.github.com>
2023-04-26 04:23:01 +0900

    Add slash command for translation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import discord
+from discord import app_commands
+from discord.ext import commands
 import deepl
 import dc_secrets as dcs # This is so I don't have to delete the token before I commit anything.
 
@@ -7,12 +9,19 @@ import dc_secrets as dcs # This is so I don't have to delete the token before I 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+activity = discord.Activity(name='and Translating...', type=discord.ActivityType.watching)
+
+client = discord.Client(intents=intents, activity=activity)
+
+tree = app_commands.CommandTree(client)
+
 
 url = "https://api-free.deepl.com/v2/translate"
-translator = deepl.Translator(dcs.deepl_auth) # dcs.deepl_auth is your DeepL API token
+translator = deepl.Translator(dcs.deepl_auth) # dcs.deepl_auth is you DeepL API token
 #TODO: Use translator.get_languages() to get a list of languages instead of hardcoding them
 val_langs = ["BG", "CS", "DA", "DE", "EL", "EN-US", "ES", "ET", "FI", "FR", "HU", "ID", "IT", "JA", "KO", "LT", "LV", "NB", "NL", "PL", "PT-BR", "PT-PT", "RO", "RU", "SL", "SV", "TR", "UK", "ZH"]
+
+
 
 @client.event
 async def on_ready():
@@ -22,8 +31,13 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+    
+    if message.content.startswith("!sync"):
+        await tree.sync(guild=discord.Object(id=889711909379641375))
+        await tree.sync()
+        return
 
-    if message.content.startswith('!trans'):
+    if message.content.startswith('!trans'): #keeping this for backwards compat, might be phased out in the future
         req = message.content.split(" ", 2)
         lang = req[1].upper()
         text = req[2]
@@ -51,5 +65,26 @@ async def on_message(message):
     if message.content.startswith('!usage'):
         usage = translator.get_usage()
         await message.channel.send(f"Character usage: {usage.character.count} of {usage.character.limit}")
+    
+
+@tree.command(name="translate", description="Translate some text")
+@app_commands.describe(message="Message to translate")
+@app_commands.describe(language="Language to translate to (default is english)")
+async def Translator(interaction: discord.Interaction, message: str = None, language: str = None):
+    await interaction.response.defer() # fix 404 no webhook bc apperently it takes over 3 seconds to respond (it doesn't it only takes like 500 ms)
+    if message == None:
+        await interaction.followup.send(content="Please provide a message")
+        return
+    if language == "EN":
+        language == "EN-US"
+    if language == None:
+        language = "EN-US"
+    language = language.upper()
+    try: # This is mostly to fix limit/bad language errors, so we just show it to the user i'll figure out a way to make it better later
+        result = translator.translate_text(message, target_lang=language)
+    except deepl.exceptions.DeepLException as e:
+        interaction.followup.send(content=e)
+    await interaction.followup.send(content=result)
+    return
         
 client.run(dcs.bot_token)
