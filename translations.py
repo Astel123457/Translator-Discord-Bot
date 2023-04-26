8f78eff34c90b9ed2f96acac8b49b1a11d006f45
Astel123457 <41224955+Astel123457@users.noreply.github.com>
2023-04-26 13:00:27 +0900

    Fix translations.py pt2

--- translations.py
+++ translations.py
@@ -1,16 +1,19 @@
-class Translations:
-    #TODO: add more languages
-    en = {
-        "translation.warning" : "Please use /translate for better compatibiliy.",
-        "message.warning" : "Please provide a message."
-    }
+#TODO: add more languages
+en = {
+    "translation.warning" : "Please use /translate for better compatibiliy.",
+    "message.warning" : "Please provide a message."
+}
     
 
-    def translate(self, lang, key):
-        lang = lang.upper()
-        if lang == "EN":
-            trans = self.en.get(key, key)
-        # If it's not a language that's supported, default to EN translations
-        else: 
-            self.en.get(key, key)
-        return trans
+def translate(lang, key):
+    lang = lang.upper()
+    trans = ""
+    if lang == "EN":
+        trans = en.get(key, key)
+    # If it's not a language that's supported, default to EN translations
+    else: 
+        trans = en.get(key, key)
+    return trans
+
+if __name__ == "__main__":
+    print("why")
