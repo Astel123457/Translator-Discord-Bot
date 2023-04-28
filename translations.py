a6b8dbb9749645cd00d8a65f5a9ffa8aa838c9c6
Astel123457 <41224955+Astel123457@users.noreply.github.com>
2023-04-28 13:43:26 +0900

    add things

--- translations.py
+++ translations.py
@@ -1,24 +1,27 @@
 #TODO: add more languages
 en = {
-    "translation.warning" : "Please use /translate for better compatibiliy.",
+    "translation.warning" : "Please use /translate for better compatibility.",
     "translation.source": "Source: ",
     "message.warning" : "Please provide a message.",
     "message.somethingwrong" : "Sorry, something went wrong. Please try again.",
-    "help.warning" : "Please specify a valid command for /help!"
+    "help.warning" : "Please specify a valid command for /help!", 
+    "help.supported" : "Here is a list of languages that I currently support: "
 }
 ja = {
     "translation.warning" : "より良い互換性のために /translate を使用してください。",
     "translation.source": "ソース: ",
     "message.warning" : "メッセージをご記入ください",
     "message.somethingwrong" : "すみません、何か変でした。もう一度お試しください。",
-    "help.warning" : "/helpに有効なコマンドを指定してください!"
+    "help.warning" : "/helpに有効なコマンドを指定してください!", 
+    "help.supported" : "Here is a list of languages that I currently support: "
 }
 fr {
-    "translation.warning" : "Please use /translate for better compatibiliy.",
+    "translation.warning" : "Please use /translate for better compatibility.",
     "translation.source": "Source: ",
     "message.warning" : "Please provide a message.",
     "message.somethingwrong" : "Sorry, something went wrong. Please try again.",
-    "help.warning" : "Please specify a valid command for /help!"
+    "help.warning" : "Please specify a valid command for /help!",
+    "help.supported" : "Here is a list of languages that I currently support: "
 }
 
 def use_translate(lang, key):
