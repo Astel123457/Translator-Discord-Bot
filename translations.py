b76f4dce47be9205db162678d2f529d326cac114
Astel123457 <41224955+Astel123457@users.noreply.github.com>
2023-04-28 13:37:06 +0900

    add japanese and non-translated french messages

--- translations.py
+++ translations.py
@@ -1,20 +1,44 @@
 #TODO: add more languages
 en = {
     "translation.warning" : "Please use /translate for better compatibiliy.",
+    "translation.source": "Source: ",
     "message.warning" : "Please provide a message.",
-    "message.somethingwrong" : "Sorry, something went wrong. Please try again."
+    "message.somethingwrong" : "Sorry, something went wrong. Please try again.",
+    "help.warning" : "Please specify a valid command for /help!"
 }
-    
+ja = {
+    "translation.warning" : "より良い互換性のために /translate を使用してください。",
+    "translation.source": "ソース: ",
+    "message.warning" : "メッセージをご記入ください",
+    "message.somethingwrong" : "すみません、何か変でした。もう一度お試しください。",
+    "help.warning" : "/helpに有効なコマンドを指定してください!"
+}
+en = {
+    "translation.warning" : "Please use /translate for better compatibiliy.",
+    "translation.source": "Source: ",
+    "message.warning" : "Please provide a message.",
+    "message.somethingwrong" : "Sorry, something went wrong. Please try again.",
+    "help.warning" : "Please specify a valid command for /help!"
+}
 
-def translate(lang, key):
+def use_translate(lang, key):
     lang = lang.upper()
     trans = ""
     if lang == "EN":
         trans = en.get(key, key)
+    
+    elif lang == "JA":
+        trans = ja.get(key, key)
+    elif lang == "FR":
+        trans = ja.get(key, key)
     # If it's not a language that's supported, default to EN translations
     else: 
         trans = en.get(key, key)
     return trans
+
+async def try_translate(*, message):
+    print(message)
+    return message
 
 if __name__ == "__main__":
     print("why")
