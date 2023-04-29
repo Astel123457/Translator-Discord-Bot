a1f6746a678084213096427360e8491be4e5df96
hako⭐ <forrestlightspeed@gmail.com>
2023-04-28 21:14:38 -0500

    refactor entire main function for console use

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import dc_secrets as dcs  # This is so I don't have to delete the token before I
 import translations as t
 import sys
 import langcodes
+import argparse
+import json
 
 # TODO: use langcodes for plaintext language to code conversion (eg "english" -> "en")
 # TODO: add translations for some pre-defined messages, like the slash command mention on line 40
@@ -117,16 +119,95 @@ async def Help(interaction: discord.Interaction, help: str = "languages", langua
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 1:
-        if sys.argv[1] == "--no-bot":
-            lang = sys.argv[2]
-            print("Lang: " + lang)
-            message = sys.argv[3:]
-            text = ""
-            for i in message:
-                text = text + i + " "
-            print("Message: " + text)
-            result = translator.translate_text(text, target_lang=lang)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--no-bot",
+        help="Don't run the Discord bot; translate in the console",
+        action="store_true"
+    )
+
+    # required lang; 1 argument only
+    parser.add_argument(
+        "lang",
+        nargs=1,
+        help="Language to translate to"
+    )
+
+    # required message;
+    # can return list if typed directly into console, or string if typed in quotes
+    # value must be sanitized later
+    parser.add_argument(
+        "message",
+        nargs="+",
+        help="Message to translate"
+    )
+
+    parser.add_argument(
+        "--usage",
+        help="Retrieve DeepL character usage quota",
+        action="store_true"
+    )
+
+    # add json output bc why not
+    parser.add_argument(
+        "--json",
+        help="Return result as JSON",
+        action="store_true"
+    )
+
+    args = parser.parse_args()
+
+    if args.usage:
+        usage = translator.get_usage()
+        print(f"Character usage: {usage.character.count} of {usage.character.limit}")
+    # continue
+
+    if args.no_bot:
+        returnjson = args.json
+        lang = args.lang[0]
+        # consider that user may have typed a name of a language instead of a code
+        # parse to a code by langcodes.find()
+        code = None
+        try:
+            code = langcodes.find(lang).language  # find language by english name, get code from .language
+        except langcodes.LanguageTagError:  # invalid tag
+            if not returnjson:
+                print("Could not parse language tag.")
+            code = "en-us"
+        except LookupError:
+            if not returnjson:
+                print(f"Could not find language \"{lang}\".")
+            code = "en-us"
+
+        if code == "en":  # dumb check
+            code = "en-us"
+
+        if code.upper() not in val_langs:
+            if not returnjson:
+                print("Sorry, DeepL doesn't support that language.")
+            code = "en-us"
+
+        if not returnjson:
+            print("Target: " + code)
+
+        # here is the message sanitization; check if list, join by space if so
+        # continues with message if string
+        message = args.message
+        if isinstance(args.message, list):
+            message = " ".join(args.message)
+        if not returnjson:
+            print("Message: " + message)
+
+        result = translator.translate_text(message, target_lang=code)
+        if returnjson:
+            resultdict = {
+                "input": message,
+                "target": code,
+                "output": str(result)
+            }
+            print(json.dumps(resultdict, indent=None))
+        else:
             print(result)
+
     else:
         bot.run(dcs.bot_token)
